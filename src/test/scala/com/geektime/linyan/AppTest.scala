7173d64d9d782e3d955cf7601240fb7e52b152c8
Lin Yan <amoyyean@hotmail.com>
2021-08-21 08:57:29 +0800

    Initial commit

---
+++ src/test/scala/com/geektime/linyan/AppTest.scala
@@ -0,0 +1,17 @@
+package com.geektime.linyan
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
