da89d14df9b201ab1d83da78aa6c1548bb9ca8c5
Lin Yan <amoyyean@hotmail.com>
2021-09-29 18:15:12 +0800

    Add CompactTable scripts

---
+++ spark_sql_hw_0908/CompactTableCommand.scala
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.command
+
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
+import org.apache.spark.sql.types.StringType
+
+case class CompactTableCommand(tableName: TableIdentifier, fileNum: Option[String])
+  extends LeafRunnableCommand {
+
+  override val output: Seq[Attribute] =
+    Seq(AttributeReference("compact", StringType)())
+
+  override def run(sparkSession: SparkSession): Seq[Row] = {
+    val table = sparkSession.table(tableName)
+
+    val partitionNum = fileNum match {
+      case None => (sparkSession.sessionState.executePlan(table.logicalPlan).
+        optimizedPlan.stats.sizeInBytes >> 7).toInt
+      case Some(value) => value.toInt
+    }
+    val tablePartitionNum = Math.max(partitionNum, 1)
+
+    val tempTableName = tableName.table + "_tmp"
+    table.repartition(tablePartitionNum)
+      .write
+      .mode(SaveMode.Overwrite)
+      .saveAsTable(tempTableName)
+
+    val tempTable = sparkSession.table(tempTableName)
+    tempTable
+      .write
+      .mode(SaveMode.Overwrite)
+      .saveAsTable(tableName.table)
+
+    sparkSession.sql(s"DROP TABLE $tempTableName;")
+    Seq(Row(s"Compact table ${tableName.table} successfully"))
+  }
+}
